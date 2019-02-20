d6aa03a68fdc1083b8e722a4c3a0c7b0cc4d7de4
Danila <danilagalkevich@gmail.com>
2019-02-20 20:27:57 +0300

    Update Hello World Test.asm

--- Hello World Test.asm
+++ Hello World Test.asm
@@ -1,91 +1,104 @@
-name "hi-world"
+data segment
+   x db 0
+  y db 0
+ 
+ends
+ 
+stack segment
+    dw   128  dup(0)
+ends
+ 
+code segment
+start:
+; set segment registers:
+    mov ax, data
+    mov ds, ax
+    mov es, ax
+    
+   mov ah,00h
+   mov al,13h
+   int 10h
+    
+ 
+push 0a000h
+pop es 
+mov al,10
+mov y,al 
+ 
+mov ax,320
+mul y 
+add ax,10
+ 
+ 
+ 
+treygolnik:
+ 
+ 
+mov dx,3c8h 
+mov al,160; 
+mov x,al 
+mov al,255
+mov y,al 
+mov ax,320;
+mul y 
+add ax,160
+mov di,ax    
 
-; this example prints out  "hello world!"
-; by writing directly to video memory.
-; in vga memory: first byte is ascii character, byte that follows is character attribute.
-; if you change the second byte, you can change the color of
-; the character even after it is printed.
-; character attribute is 8 bit value,
-; high 4 bits set background color and low 4 bits set foreground color.
-
-; hex    bin        color
+mov cx,20
+left:
+    mov byte ptr es:[di],4
+    add di,320    
+    loop left
+    
+mov cx,50
+down:
+    mov byte ptr es:[di],4
+    inc di    
+    loop down
+    
+mov cx,20
+right:
+    mov byte ptr es:[di],4
+    sub di,320    
+    loop right
+    
+mov cx,50    
+top:
+    mov byte ptr es:[di],4
+    dec di    
+    loop top         
+       
+;cycle11:
+;    mov byte ptr es:[di],4
+;    add di,320;
+;      
+;    loop cycle11
+;    mov cx,50      ;
 ; 
-; 0      0000      black
-; 1      0001      blue
-; 2      0010      green
-; 3      0011      cyan
-; 4      0100      red
-; 5      0101      magenta
-; 6      0110      brown
-; 7      0111      light gray
-; 8      1000      dark gray
-; 9      1001      light blue
-; a      1010      light green
-; b      1011      light cyan
-; c      1100      light red
-; d      1101      light magenta
-; e      1110      yellow
-; f      1111      white
+;cycl21:
+; 
+;    mov byte ptr es:[di],10 
+;    inc di  
+;    loop cycl21 
+; 
+;    mov cx,50      ;
+; 
+;cyce31:
+;    mov byte ptr es:[di],9 
+;    sub di,320
+;    dec di  
+;    loop cyce31
  
-
-
-org 100h
-
-; set video mode    
-mov ax, 3     ; text mode 80x25, 16 colors, 8 pages (ah=0, al=3)
-int 10h       ; do it!
-
-; cancel blinking and enable all 16 colors:
-mov ax, 1003h
-mov bx, 0
-int 10h
-
-
-; set segment register:
-mov     ax, 0b800h
-mov     ds, ax
-
-; print "hello world"
-; first byte is ascii code, second byte is color code.
-
-mov [02h], 'H'
-
-mov [04h], 'e'
-
-mov [06h], 'l'
-
-mov [08h], 'l'
-
-mov [0ah], 'o'
-
-mov [0ch], ','
-
-mov [0eh], 'W'
+        mov ah,08h
+    int 21h
+  
  
-mov [10h], 'o'
-
-mov [12h], 'r'
-
-mov [14h], 'l'
-
-mov [16h], 'd'
-
-mov [18h], '!'
-
-
-
-
-; color all characters:
-mov cx, 12  ; number of characters.
-mov di, 03h ; start from byte after 'h'
-
-c:  mov [di], 11101100b   ; light red(1100) on yellow(1110)
-    add di, 2 ; skip over next ascii code in vga memory.
-    loop c
-
-; wait for any key press:
-mov ah, 0
-int 16h
-
-ret
-
+    mov ah,00h
+    mov al,03h
+    int 10h
+ 
+    mov ax, 4c00h ; exit to operating system.
+    int 21h    
+ends
+ 
+end start ; set entry point and stop the assembler.
